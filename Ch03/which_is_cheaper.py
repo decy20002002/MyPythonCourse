e2894800fa99a2862d31c519ccd8376a8a570211
decy20002002 <decy20002002@yahoo.com>
2019-09-24 13:01:05 -0700

    s

--- Ch03/which_is_cheaper.py
+++ Ch03/which_is_cheaper.py
@@ -14,8 +14,8 @@ print(price_per_ounce1)
 print('Price per ounce of product 2:')
 print(price_per_ounce2)
 print(price_per_ounce1 < price_per_ounce2)
-if False
-print(str"Good!")
+False
+print('Good!')
 # print('Product 1 is an integer:' , price1.is_integer())
 
 # input()
