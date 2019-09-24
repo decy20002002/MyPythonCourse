cc7d91ddaf48272b0d90bd0003bdb23ca86e9fbb
decy20002002 <decy20002002@yahoo.com>
2019-09-24 09:49:59 -0700

    s

--- Ch03/Basic.py
+++ Ch03/Basic.py
@@ -1,6 +1,5 @@
 import sys
-# x = "Hello"
-# y = "and Welcome to a basice python program"
-print('You just ran the program named', sys.argv[0])
-print('Greetings, ' + sys.argv[1] + 'and welcome to a basic python program')
+
+food - input("What is your favorite food?")
+print("You lkie " + food + "? I might like it too")
 input()
