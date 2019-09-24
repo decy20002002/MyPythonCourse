3427adcd8c0bd4fd4ca9c1d56defd953612193f9
decy20002002 <decy20002002@yahoo.com>
2019-09-24 13:09:57 -0700

    s

--- Ch03/which_is_cheaper.py
+++ Ch03/which_is_cheaper.py
@@ -1,21 +1,22 @@
 import sys
 import math
 price1 = float(input("What is the price of product 1? "))
-weight1 = float(input("How many ounces is produc 1? "))
+# weight1 = float(input("How many ounces is produc 1? "))
 
 price2 = float(input("What is the price of product 2? "))
-weight2 = float(input("How many ounces is produc 2? "))
+# weight2 = float(input("How many ounces is produc 2? "))
 
-price_per_ounce1 = price1 / weight1
-price_per_ounce2 = price2 / weight2
+# price_per_ounce1 = price1 / weight1
+# price_per_ounce2 = price2 / weight2
 
-print('Price per ounce of product 1:')
-print(price_per_ounce1)
-print('Price per ounce of product 2:')
-print(price_per_ounce2)
-print(price_per_ounce1 < price_per_ounce2)
-True
-print('Good!')
-# print('Product 1 is an integer:' , price1.is_integer())
+
+# print('Price per ounce of product 1:')
+# print(price_per_ounce1)
+# print('Price per ounce of product 2:')
+# print(price_per_ounce2)
+# print(price_per_ounce1 < price_per_ounce2)
+
+print('Original Price of Product 1:' , price1)
+print('Original Price of Product 2:' , price2)
 
 # input()
