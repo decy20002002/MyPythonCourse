7bbf885a6b66369e9a6d7146669403487e52ee75
decy20002002 <decy20002002@yahoo.com>
2019-09-24 13:15:54 -0700

    s

--- Ch03/which_is_cheaper.py
+++ Ch03/which_is_cheaper.py
@@ -1,14 +1,13 @@
 import sys
 import math
 price1 = float(input("What is the price of product 1? "))
-# weight1 = float(input("How many ounces is produc 1? "))
+discount1 = int(input("Produc 1 discount? "))
 
 price2 = float(input("What is the price of product 2? "))
-# weight2 = float(input("How many ounces is produc 2? "))
+discount2 = int(input("Produc 2 discount? "))
 
-# price_per_ounce1 = price1 / weight1
-# price_per_ounce2 = price2 / weight2
-
+product1_final_price = price1 - (price1 * discount1 / 100)
+product2_final_price = price2 - (price2 * discount2 / 100)
 
 # print('Price per ounce of product 1:')
 # print(price_per_ounce1)
@@ -17,6 +16,8 @@ price2 = float(input("What is the price of product 2? "))
 # print(price_per_ounce1 < price_per_ounce2)
 
 print('Original Price of Product 1:' , price1)
+print('New Price of Product 1 after 10% discount:' , price11)
 print('Original Price of Product 2:' , price2)
+print('New Price of Product 2 after 10% discount:' , price22)
 
 # input()
