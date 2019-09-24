abf6ef417cae87a17a538b159dd2b4da63ea4492
decy20002002 <decy20002002@yahoo.com>
2019-09-24 11:48:39 -0700

    s

--- Ch03/which_is_cheaper.py
+++ Ch03/which_is_cheaper.py
@@ -2,5 +2,5 @@ import sys
 
 product1_price = input("What is the price of product 1? ")
 # print('the type is', type(product1_price))
-print(float('product1_price is:'))
+print('product1_price')
 input()
