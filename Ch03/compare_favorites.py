4aeec8d9c07075a0180aa4f2bbfe28dabbb273be
decy20002002 <decy20002002@yahoo.com>
2019-09-24 15:10:04 -0700

    s

--- Ch03/compare_favorites.py
+++ Ch03/compare_favorites.py
@@ -1,3 +1,7 @@
 food = input("What is your favorite food? ")
 if (food == "pizza"):
     print("Pizza is my favorite food")
+elif (food == "tacos"):
+    print("Tacos is my 2nd favorite") 
+else:
+    print("You like " + food + "? I might like it too")
