12da0b0ea6ce1666323d3bf2337281151d2e5434
decy20002002 <decy20002002@yahoo.com>
2019-09-24 09:47:42 -0700

    s

--- Ch03/Basic.py
+++ Ch03/Basic.py
@@ -2,5 +2,5 @@ import sys
 # x = "Hello"
 # y = "and Welcome to a basice python program"
 print('You just ran the program named', sys.argv[0])
-print('Greetings, ' + sys.argv[1])
+print('Greetings, ' + sys.argv[1] + 'and welcome to a basic python program')
 input()
