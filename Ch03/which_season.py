4eef058d72dfee8a361a1cb152889f077dc66ea0
decy20002002 <decy20002002@yahoo.com>
2019-09-24 15:41:29 -0700

    s

--- Ch03/which_season.py
+++ Ch03/which_season.py
@@ -8,15 +8,22 @@ day_string = input("input the day of the month (e.g. 19): ")
 month = int(month_string)
 day = int(day_string)
 
-if ((month == 1 or month == 2 or month == 3 or month == 12) and day > 20):
+if month ==1 or month==2 or month==3:
     	season = 'winter'
-elif ((month == 4 or month==5 or month==6) and day > 19):
+elif month == 4 or month==5 or month==6:
 	season = 'spring'
-elif ((month ==7 or month==8 or month==9) and day > 20):
+elif month ==7 or month==8 or month==9:
 	season = 'summer'
-elif (month == 9 and day > 21):
-    season = 'autumn'
 else:
-	sys.exit(-1)
+	season = 'autumn'
 
-print("Season is", season)
+if (month == 3) and (day > 19):
+	season = 'spring'
+elif (month == 6) and (day > 20):
+	season = 'summer'
+elif (month == 9) and (day > 21):
+	season = 'autumn'
+elif (month == 12) and (day > 20):
+	season = 'winter'
+
+print("Season is",season)
