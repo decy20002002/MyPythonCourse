b7941d7b636b73dc7a75e5640da85c029803fc50
decy20002002 <decy20002002@yahoo.com>
2019-09-24 14:10:22 -0700

    s

--- Ch03/string_practice.py
+++ Ch03/string_practice.py
@@ -4,7 +4,7 @@ import math
 print('Let\'s code some Python')
 print("That's when she said, \"Everyone doesn't understand!\"")
 print("Here are 3 backslashes: ", "\ ", "\ ", "\ ")
-haggis_poem = 'Much to his dad and mum\'s dismay' \n
+haggis_poem = 'Much to his dad and mum\'s dismay'/n
                 'Horace ate himself one day'
 
 print(haggis_poem)
