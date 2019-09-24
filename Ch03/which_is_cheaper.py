059f2235ea702943a833301924a597ac1d3b8be0
decy20002002 <decy20002002@yahoo.com>
2019-09-24 12:47:34 -0700

    s

--- Ch03/which_is_cheaper.py
+++ Ch03/which_is_cheaper.py
@@ -1,12 +1,13 @@
 import sys
 import math
-
 product1_price = input("What is the price of product 1? ")
-# print('the type is', type(product1_price))
+weight1 = float(input("How many ounces is produc 1? "))
 
+product1_price = input("What is the price of product 2? ")
+weight2 = float(input("How many ounces is produc 2? "))
 price1 = float(product1_price)
 
 # print('Your product price is ', price1)
-print('Product 1 is an integer:' , price1.is_integer())
+# print('Product 1 is an integer:' , price1.is_integer())
 
 # input()
