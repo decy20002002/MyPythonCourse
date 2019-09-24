4a454b431e6aeeb29bad8773c8725fd750a26453
decy20002002 <decy20002002@yahoo.com>
2019-09-24 13:55:22 -0700

    s

--- Ch03/string_practice.py
+++ Ch03/string_practice.py
@@ -2,4 +2,4 @@ import sys
 import math
 
 print('Let\'s code some Python')
-print("That's when she said, \\"Everyone doesn't understand!"")
+print("That's when she said, \"Everyone doesn't understand!\"")
