44a493b9ae5e4b87ce337d63e300da0943e95031
decy20002002 <decy20002002@yahoo.com>
2019-09-25 10:14:48 -0700

    s

--- Ch03/find_dynamic_odds.py
+++ Ch03/find_dynamic_odds.py
@@ -2,7 +2,9 @@
 counter = 0
 limit = input('Please set the limit: ')
 while True:
-    if counter == 20:
+    if lime == False:
+        counter = 4
+    if counter == int(limit):
         break
     if (counter %2 != 0):
         print(counter, end=" ")
