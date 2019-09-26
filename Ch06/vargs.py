11a38dee3b49be1f6d67d707d0b6d2880027d91f
decy20002002 <decy20002002@yahoo.com>
2019-09-26 13:52:52 -0700

    s

--- Ch06/vargs.py
+++ Ch06/vargs.py
@@ -15,4 +15,7 @@ def slice_list(list_to_slice, *upper_bounds):
 
 some_nums = [2,6,4,2,22,54,12,8,-1]
 
+print('Value returned',slice_list(some_nums,4))
+print('Value returned',slice_list(some_nums,12))
+
 print(slice_list(some_nums, 12,7,2,3))
