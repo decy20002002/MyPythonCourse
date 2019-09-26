dd45e508ab4e4aa63e56ff9f3f393d0a769e28e0
decy20002002 <decy20002002@yahoo.com>
2019-09-26 16:15:07 -0700

    s

--- Ch07/bonus_user_badinput.py
+++ Ch07/bonus_user_badinput.py
@@ -2,10 +2,10 @@ import sys
 
 #try:
     
-num1 = input("Enter num1: ")
-num2 = input("Enter num2: ")
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
     
-Division = num1/num2
+Division = num1 / num2
     
 print('Total number after division:', Division)
 #except print('Enter num1: ', input(num1)):
