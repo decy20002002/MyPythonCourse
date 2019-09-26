63d54f89ffc180b5e2a2d98b7d93fc1042a61890
decy20002002 <decy20002002@yahoo.com>
2019-09-26 13:50:12 -0700

    s

--- Ch06/vargs.py
+++ Ch06/vargs.py
@@ -5,14 +5,14 @@ def slice_list(list_to_slice, *upper_bounds):
     list_to_return = []
 
     for upper_bound in upper_bounds:
-    if(len(list_to_slice)>upper_bound):
-        print('Slicing...')
-        list_to_return.append(list_to_slice[:upper_bound])
-    else:
-        list_to_return.append(None)
+        if(len(list_to_slice)>upper_bound):
+            print('Slicing...')
+            list_to_return.append(list_to_slice[:upper_bound])
+        else:
+            list_to_return.append(None)
+        
     return list_to_return
 
 some_nums = [2,6,4,2,22,54,12,8,-1]
 
-print('Value returned',slice_list(some_nums,4))
-print('Value returned',slice_list(some_nums,12))
+print(slice_list(some_nums, 12,7,2,3))
