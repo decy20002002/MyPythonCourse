e1ef6f549eb5a410e3267a700a50bc0be602ad60
decy20002002 <decy20002002@yahoo.com>
2019-09-26 16:18:17 -0700

    s

--- Ch07/bonus_user_badinput.py
+++ Ch07/bonus_user_badinput.py
@@ -1,12 +1,13 @@
 import sys
 
-#try:
+try:
     
-num1 = float(input("Enter num1: "))
-num2 = float(input("Enter num2: "))
+    num1 = float(input("Enter num1: "))
+    num2 = float(input("Enter num2: "))
+    Division = num1 / num2
     
-Division = num1 / num2
-    
-print('Total number after division:', Division)
-#except print('Enter num1: ', input(num1)):
+except ZeroDivisionError:
+    print('Divide by ZERO is unacceptable!')
+    sys.exit(-1)
 
+print('Total number after division:', Division)
