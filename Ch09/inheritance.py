75371a9cc800182ad21859e790d6df8bd6c181c4
decy20002002 <decy20002002@yahoo.com>
2019-09-27 12:43:47 -0700

    s

--- Ch09/inheritance.py
+++ Ch09/inheritance.py
@@ -6,7 +6,18 @@ class Customer:
         self.id = id
 
     def print_info(self):
-        print(f"Customer id #{self.id}: {self.name}")
+        print(f"Customer id: {self.id}. {self.name}")
 
 c = Customer('Frank',111)
-c.print_info
+c.print_info()
+
+class PrimeCustomer(Customer):
+    def __init__(self, name, id, expiration):
+        super().__init__(name, id)
+        self.expiration = expiration
+
+    def print_info(self):
+        print(f"Prime membership expires {self.expiration} ", end="")
+        super().print_info()
+p = PrimeCustomer("Betty", 222, "12.15.2021")
+p.print_info()
